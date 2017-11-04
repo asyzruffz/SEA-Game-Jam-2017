7aad170ffd68be894db137819e3a6622c356bdce
Barli J <barli_mmc@hotmail.com>
2017-11-04 18:40:31 +0800

    + basic load scene + exit application

--- Assets/Scripts/Title/TitleController.cs
+++ Assets/Scripts/Title/TitleController.cs
@@ -6,8 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour 
 {
+	// public int sceneIndex;
+
 	void Start () 
 	{
 		
+	}
+
+	public void PlayButton(int _index)
+	{
+		SceneManager.LoadSceneAsync(_index);
+	}
+
+	public void ExitButton()
+	{
+		Application.Quit();
+		Debug.Log("Exit Application");
 	}
 }
