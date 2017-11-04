7771fb2aff62fb3857d07769347ea47a91ea6b23
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-11-04 12:36:04 +0800

    Added overview of player and tiles

---
+++ Assets/Scripts/FloorTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTile : MonoBehaviour {
+
+	public FloorTile upTile;
+	public FloorTile downTile;
+	public FloorTile rightTile;
+	public FloorTile leftTile;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	public bool IsUpValid () {
+		return upTile != null;
+	}
+
+	public bool IsDownValid () {
+		return downTile != null;
+	}
+
+	public bool IsRightValid () {
+		return rightTile != null;
+	}
+
+	public bool IsLeftValid () {
+		return leftTile != null;
+	}
+}
