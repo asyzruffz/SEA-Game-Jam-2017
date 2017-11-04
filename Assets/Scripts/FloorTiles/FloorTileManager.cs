dc391cb26004aad5ec338b2fbdbcbc8122a0986e
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-11-04 22:18:38 +0800

    Added corridor generation

--- Assets/Scripts/FloorTiles/FloorTileManager.cs
+++ Assets/Scripts/FloorTiles/FloorTileManager.cs
@@ -11,11 +11,13 @@ public class FloorTileManager : MonoBehaviour {
 	public float generateDelay = 1;
 	public bool initFirstTile;
 	public float offsetDistance = 1;
+	public int corridorLength = 10;
 	public GameObject prefabNormalTile;
 	public GameObject prefabSpecialTile;
-	public List<TilesPattern> patterns = new List<TilesPattern> ();
+	public List<TilesPattern> hallPatterns = new List<TilesPattern> ();
 
 	FloorTile lastTileGenerated = null;
+	FloorTile lastMiddleGenerated = null;
 	FloorTile lastLeftGenerated = null;
 	FloorTile lastRightGenerated = null;
 	float timer = 0;
@@ -46,7 +48,14 @@ public class FloorTileManager : MonoBehaviour {
 			} else if (Input.GetKeyDown (KeyCode.K)) {
 				GenerateRelativeToTileAt (lastTileGenerated, TileDirection.Right);
 			} else if (Input.GetKeyDown (KeyCode.Space)) {
-				GenerateHallForward (lastTileGenerated);
+				GenerateHallForward (lastMiddleGenerated == null ? lastTileGenerated : lastMiddleGenerated);
+			} else if (Input.GetKeyDown (KeyCode.B)) {
+				FloorTile exit = GetEmptyHallEndTile ();
+				if (exit != null) {
+					GenerateCorridorForward (exit);
+				} else {
+					Debug.Log ("No empty tiles to exit hall into corridor!");
+				}
 			}
 		}
 	}
@@ -54,7 +63,7 @@ public class FloorTileManager : MonoBehaviour {
 	void GenerateHallForward (FloorTile refTile) {
 		FloorTile anchor = refTile;
 
-		TilesPattern randomPattern = patterns[Random.Range(0, patterns.Count)];
+		TilesPattern randomPattern = hallPatterns[Random.Range(0, hallPatterns.Count)];
 
 		for (int i = 0; i < randomPattern.pattern.Count; i++) {
 			GenerateRelativeToTileAt (anchor, TileDirection.Up, randomPattern.pattern[i].col2);
@@ -74,8 +83,57 @@ public class FloorTileManager : MonoBehaviour {
 			lastTileGenerated.downTile = lastRightGenerated;
 			lastRightGenerated = lastTileGenerated;
 		}
-		
-		lastTileGenerated = anchor;
+
+		lastMiddleGenerated = anchor;
+	}
+
+	void GenerateCorridorForward (FloorTile refTile) {
+		GenerateRelativeToTileAt (refTile, TileDirection.Up);
+		int lastTurnCount = 1;
+
+		TileDirection oldDir = TileDirection.Up;
+		for (int i = 1; i < corridorLength - 1; i++) {
+			TileDirection dir = oldDir;
+			if (lastTurnCount >= 2) {
+				if (Random.value < 0.5f) {
+					dir = TileDirection.Up;
+				} else {
+					if (oldDir == TileDirection.Left || oldDir == TileDirection.Right) {
+						dir = oldDir;
+					} else {
+						dir = (Random.value < 0.5f) ? TileDirection.Left : TileDirection.Right;
+					}
+				}
+			}
+			
+			GenerateRelativeToTileAt (lastTileGenerated, dir);
+			lastTurnCount = (dir == oldDir) ? lastTurnCount + 1 : 0;
+			oldDir = dir;
+		}
+
+		GenerateRelativeToTileAt (lastTileGenerated, TileDirection.Up);
+		lastMiddleGenerated = lastTileGenerated;
+		lastLeftGenerated = lastRightGenerated = null;
+	}
+
+	FloorTile GetEmptyHallEndTile () {
+		List<FloorTile> endHallTiles = new List<FloorTile> ();
+
+		if (lastMiddleGenerated != null && !lastMiddleGenerated.isSpecial) {
+			endHallTiles.Add (lastMiddleGenerated);
+		}
+		if (lastLeftGenerated != null && !lastLeftGenerated.isSpecial) {
+			endHallTiles.Add (lastLeftGenerated);
+		}
+		if (lastRightGenerated != null && !lastRightGenerated.isSpecial) {
+			endHallTiles.Add (lastRightGenerated);
+		}
+
+		if (endHallTiles.Count > 0) {
+			return endHallTiles[Random.Range(0, endHallTiles.Count)];
+		} else {
+			return null;
+		}
 	}
 
 	void GenerateRelativeToTileAt (FloorTile refTile, TileDirection direction, bool isSpecialTile = false) {
@@ -120,5 +178,6 @@ public class FloorTileManager : MonoBehaviour {
 		newTile.transform.localPosition = refPos + offset;
 
 		lastTileGenerated = newTile.GetComponent<FloorTile> ();
+		lastTileGenerated.GetComponent<FloorTile> ().isSpecial = isSpecialTile;
 	}
 }
